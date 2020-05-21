4b9a71956e7a773f4bfd7a3f4ddb255100dd7a8f
nicol-hawkins <nicolchawkins@gmail.com>
2020-05-20 20:54:03 -0700

    added methods for toggling bars

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Bar from './components/Bar/Bar.js';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+class App extends Component () {
+  state = {
+    isVisible: false
+  }
+
+  showBar = () => {
+    this.setSate({
+      isVisible: true
+    });
+  }
+
+  hideBar = () => {
+    this.setState({
+      isVisible: false
+    });
+  }
+
+
+
+  render(){
+    return (
+      <div className="App">
+
+        <div>
+          <Bar
+            show={this.state.isVisible}
+            onShow={this.showBar}
+            onHide={this.hideBar}
+            text={"see more"}>
+
+          </Bar>
+          </div>
+        
+      </div>
+    );
+  }
 }
 
 export default App;
