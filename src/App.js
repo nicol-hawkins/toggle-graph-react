faad875771a3f6fccfb29de3a3ace65608444ecb
nicol-hawkins <nicolchawkins@gmail.com>
2020-06-12 14:31:57 -0700

    removed country buttons for simple version

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
 
-import CountryList from './components/CountryList/CountryList.js';
 import TitleBar from './components/TitleBar/TitleBar';
 import BarChart from './components/BarChart/BarChart';
 
@@ -10,8 +9,7 @@ class App extends Component {
   state = {
     isVisible: false,
     year: "2016",
-    data: [],
-    chosenCountries: []
+    data: []
   };
 
   componentDidMount() {
@@ -21,7 +19,6 @@ class App extends Component {
         this.setState({
           data: data[this.state.year]
         });
-        console.log('got the data!', this.state.year, this.state.data)
       });
   };
 
@@ -29,56 +26,10 @@ class App extends Component {
     let year = ev.target.value;
     this.setState({
       year: year       
-    }, () => {console.log('Current Year: ', this.state.year)})
+    }, () => {})
     this.componentDidMount()
   };
 
-  showBar = (data, info, index) => {
-    // console.log('Show Bar')
-    // const chosenCountries = this.state.chosenCountries
-    // const chosenCountry = data.Country;
-    // chosenCountries.push(chosenCountry);
-
-    // console.log('Country Toggled: ', chosenCountry)
-    // console.log('Chosen Countries: ', chosenCountries)
-    // console.log(data.Country)
-    // this.chooseCountry()
-    // console.log('Index of Country: ', data)
-    // console.log('Name of Country: ', )
-
-    
-
-    this.setState({
-      isVisible: !this.state.isVisible
-    });
-  };
-
-  // chooseCountry = (index) => {
-  //   //slicing/splice method
-  //   const chosenCountries = this.state.chosenCountries.slice();
-  //   const availableCountries = this.props.data.slice();
-
-  //   //retrieve country of choice
-  //   const chosenCountry = availableCountries[index];
-  //   console.log(chosenCountry)
-
-  //   //add to chosen list
-  //   chosenCountries.push(chosenCountry);
-  //   availableCountries.splice(index, 1);
-  //   console.log('Chosen Countries: ', chosenCountries)
-
-  //   this.setState({
-  //     chosenCountries: [...this.state.chosenCountries, chosenCountry]
-  //   })    
-  
-  // };
-
-  hideBar = () => {
-    console.log('Hide Bar')
-    this.setState({
-      isVisible: !this.state.isVisible
-    })
-  };
 
   render(){
     return (
@@ -91,25 +42,16 @@ class App extends Component {
         </div>
     
         <div className="MainContainer">
-          <CountryList
-            isVisible={this.state.isVisible}
-            onShow={this.showBar}
-            onHide={this.hideBar}
-            data={this.state.data}>
-          </CountryList>
-
           <div className="BarChart" id="results">
-            {
-              this.state.isVisible ?
+            {              
               this.state.data.map((info, index) => (
                 <BarChart
                   className="bar--show bar"
                   info={info}
                   data={this.state.data}
                   year={this.state.year}
-                  key={index}
-                  chosenCountries={this.state.chosenCountries}/>                
-              )) : null
+                  key={index}/>                
+              )) 
             }
           </div>
       </div>
